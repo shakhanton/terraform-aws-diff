5cb04bf2c3be8f0a9641ef0fb2b7bfe5bcce1279
Anton Shakh <shakh@quintagroup.org>
2020-06-22 01:21:00 +0300

    refactor code

--- main.py
+++ main.py
@@ -2,125 +2,62 @@ import json
 import boto3
 from flatten_json import flatten
 
-client = boto3.client('iam')
-
-list_user = []
-list_roles = []
-list_policy = []
-list_InstanceProfile = []
-list_groups = []
-list_dashboards = []
-list_Topics = []
-list_describe_db_instances = []
-list_functions = []
-list_aws_arn = []
-
-paginator = client.get_paginator('list_users')
-for response in paginator.paginate():
-
-    for aws_arn in response['Users']:
-        list_user.append(aws_arn['Arn'])
-    user = response.get('Users')
+def get_aws_arn(service, operation, service_object, object_arn, **paginate_params):
+    arns = []
+    client = boto3.client(service)
+    paginator = client.get_paginator(operation)
+    page_iterator = paginator.paginate(**paginate_params)
+    for page in page_iterator:
+        for arn in page[service_object]:
+            arns.append(arn[object_arn])
+    return arns
 
 
-roles = client.list_roles()
-Role_list = roles['Roles']
-for key in Role_list:
-    list_roles.append(key['Arn'])
+def get_state_arns(state_file):
+    list_state_arn = []
+    with open(state_file, 'r') as f:
+        data = json.load(f)
 
-paginator = client.get_paginator('list_policies')
-for response in paginator.paginate(Scope='Local'):
+    flat_json = flatten(data)
+    # print(json.dumps(flat_json, indent=4))
+    # ARN's from state
 
-    for aws_arn in response['Policies']:
-        list_policy.append(aws_arn['Arn'])
+    for key, value in list(flat_json.items()):
+        if key.startswith('prior_state_values_'):
+            del flat_json[key]
 
-paginator = client.get_paginator('list_instance_profiles')
-for response in paginator.paginate():
-
-    for aws_arn in response['InstanceProfiles']:
-        list_InstanceProfile.append(aws_arn['Arn'])
-
-paginator = client.get_paginator('list_groups')
-for response in paginator.paginate():
-
-    for aws_arn in response['Groups']:
-        list_groups.append(aws_arn['Arn'])
-
-client = boto3.client('cloudwatch')
-
-paginator = client.get_paginator('list_dashboards')
-for response in paginator.paginate():
-
-    for aws_arn in response['DashboardEntries']:
-        list_dashboards.append(aws_arn['DashboardArn'])
-
-client = boto3.client('sns')
-
-paginator = client.get_paginator('list_topics')
-for response in paginator.paginate():
-
-    for aws_arn in response['Topics']:
-        list_Topics.append(aws_arn['TopicArn'])
-
-client = boto3.client('rds')
-
-paginator = client.get_paginator('describe_db_instances')
-for response in paginator.paginate():
-
-    for aws_arn in response['DBInstances']:
-        list_describe_db_instances.append(aws_arn['DBInstanceArn'])
-
-client = boto3.client('lambda')
-
-paginator = client.get_paginator('list_functions')
-for response in paginator.paginate():
-
-    for aws_arn in response['Functions']:
-        list_functions.append(aws_arn['FunctionArn'])
-
-with open('plan.json') as f:
-  data = json.load(f)
-
-flat_json = flatten(data)
-
-# ARN's from state
-for key, value in flat_json.items():   # iter on both keys and values
+    for key, value in flat_json.items():   # iter on both keys and values
         if key.endswith('_arn'):
-                list_aws_arn.append(value)
+            list_state_arn.append(value)
+    return list_state_arn
+
+list_state = get_state_arns('plan.json')
+
+### prior_state_values_
+
+def compare_function(resource_type_desc, list_aws):
+
+    resources_in_aws_only = [item for item in list_aws if item not in list_state]
+    print(f"\033[1m{resource_type_desc} not under terraform \033[0m", *resources_in_aws_only, sep="\n")
 
 
-iam_users = [item for item in list_user if item not in list_aws_arn]
-print("IAM users not under terraform")
-print(iam_users)
+list_aws_user = get_aws_arn('iam', 'list_users', 'Users', 'Arn')
+list_roles = get_aws_arn('iam', 'list_roles', 'Roles', 'Arn')
+list_policy = get_aws_arn('iam', 'list_policies', 'Policies', 'Arn', Scope='Local')
+list_instance_profile = get_aws_arn('iam', 'list_instance_profiles', 'InstanceProfiles', 'Arn')
+list_groups = get_aws_arn('iam', 'list_groups', 'Groups', 'Arn')
+list_dashboards = get_aws_arn('cloudwatch', 'list_dashboards', 'DashboardEntries', 'DashboardArn')
+list_topics = get_aws_arn('sns', 'list_topics', 'Topics', 'TopicArn')
+list_describe_db_instances = get_aws_arn('rds', 'describe_db_instances', 'DBInstances', 'DBInstanceArn')
+list_functions = get_aws_arn('lambda', 'list_functions', 'Functions', 'FunctionArn')
 
-iam_roles = [item for item in list_roles if item not in list_aws_arn]
-print("IAM Roles not under terraform")
-print(iam_roles)
 
-iam_policy = [item for item in list_policy if item not in list_aws_arn]
-print("IAM Policies not under terraform")
-print(iam_policy)
-
-iam_InstanceProfile = [item for item in list_InstanceProfile if item not in list_aws_arn]
-print("IAM InstanceProfile not under terraform")
-print(iam_InstanceProfile)
-
-iam_groups = [item for item in list_groups if item not in list_aws_arn]
-print("IAM InstanceProfile not under terraform")
-print(iam_groups)
-
-cloudwatch_dashboards = [item for item in list_dashboards if item not in list_aws_arn]
-print("Cloudwatch dashboards not under terraform")
-print(cloudwatch_dashboards)
-
-sns_topics = [item for item in list_Topics if item not in list_aws_arn]
-print("SNS topics not under terraform")
-print(sns_topics)
-
-rds_db_instances = [item for item in list_describe_db_instances if item not in list_aws_arn]
-print("RDS DB Instances not under terraform")
-print(rds_db_instances)
-
-lambda_functions = [item for item in list_functions if item not in list_aws_arn]
-print("IAM Lambda Functions not under terraform")
-print(lambda_functions)
+compare_function("IAM Users", list_aws_user)
+compare_function("IAM Roles", list_roles)
+compare_function("IAM Policies", list_policy)
+compare_function("IAM InstanceProfile", list_instance_profile)
+compare_function("IAM Groups", list_groups)
+compare_function("Cloudwatch dashboards", list_dashboards)
+compare_function("SNS topics", list_topics)
+compare_function("RDS DB Instances", list_describe_db_instances)
+compare_function("IAM Lambda Functions", list_functions)
